a75aeeca6652bc178e626100909f886a60c5ca0f
Ikkiw-dev <pongsatorn.liao@kmutt.ac.th>
2026-01-22 16:52:17 +0700

    Class2

---
+++ lec02 - class/forloop.py
@@ -0,0 +1,16 @@
+# object = ["A","B","C","D","E","F"]
+#
+# for var in range(len(object)):
+#     print("Hello " + object[var])
+# print("End")
+#
+# for i in range(1, 20, 2):
+#     print(i, end = " ")
+
+#Exercise
+
+#ans key
+for i in range(1,6):
+    print("{}".format("*"*i))
+
+
---
+++ lec02 - class/whileloop.py
@@ -0,0 +1,28 @@
+# #Task 1
+# i = 100
+# while i >= 1:
+#     print(i, end = " ") #set format
+#     i-=1
+
+
+#Task 2
+# j = 9
+# while j >= 1:
+#     print(j, end = " ")
+#     j = j - 2
+
+#Task 3
+# k = 1
+# while k <= 70:
+#     print(k, end = " ")
+#     k = k*2
+
+#Task 4
+l = 10
+while l >= 1:
+    print(l, end = ",")
+    if l % 2 == 0:
+        print("x,", end = "")
+    else:
+        print("y,", end = "")
+    l = l-1
